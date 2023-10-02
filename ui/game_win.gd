3f0eccdd1a67f5fc7f8d702ed1beecf7a9c23af6
EliasVincent <riesyeti@outlook.de>
2023-10-02 22:42:36 +0200

    win now shows time left

--- ui/game_win.gd
+++ ui/game_win.gd
@@ -2,10 +2,13 @@ extends Control
 
 @onready var main_menu = $VBoxContainer/MainMenu
 @onready var quit = $VBoxContainer/Quit
+@onready var time = $time
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	main_menu.grab_focus()
+	GlobalLogic.game_timer.paused = true
+	time.text = str("TIME LEFT: ", round(GlobalLogic.game_timer.time_left))
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
